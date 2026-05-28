1bd3bd028a96806ae1d48d3839e5a7445c8ce77a
kiran more <kiransmore2003@gmail.com>
2026-05-28 11:37:10 +0530

    changes added

---
+++ Gitchange/Controllers/HomeController2.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gitchange.Controllers
+{
+    public class HomeController2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
